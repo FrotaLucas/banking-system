8e7a066d24e828e8790cc37a9e3a71a8747e7e9e
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-20 00:41:38 +0200

    iplement intity

--- BankingSystem.Domain/Entities/Account.cs
+++ BankingSystem.Domain/Entities/Account.cs
@@ -2,5 +2,10 @@
 {
     public class Account
     {
+        public int AccountId { get; set; }
+        public string AccountNumber { get; set; }
+        public int CustomerId { get; set; }
+        public decimal OpeningBalance { get; set; }
+        //public decimal CurrentBalance { get; set; } pq ????
     }
 }
