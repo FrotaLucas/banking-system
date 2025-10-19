cb32535303facb88686e969db9ea991a61f46cf5
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-20 00:26:18 +0200

    implement reference of Domain

--- BankingSystem.Infrastructure/Repositories/AccountRepository.cs
+++ BankingSystem.Infrastructure/Repositories/AccountRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace BankingSystem.Infrastructure.Repositories
+﻿using BankingSystem.Domain.Entities;
+using BankingSystem.Domain.Interfaces.IRepositories;
+
+namespace BankingSystem.Infrastructure.Repositories
 {
-    public class AccountRepository
+    public class AccountRepository : IRepositoryAccount
     {
+        public List<Account> GetAccounts()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
