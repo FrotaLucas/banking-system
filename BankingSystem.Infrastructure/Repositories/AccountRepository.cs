2087ff959fe0fbe193ac70f327ead4dec18673b4
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-20 00:34:57 +0200

    implement methods of interface

--- BankingSystem.Infrastructure/Repositories/AccountRepository.cs
+++ BankingSystem.Infrastructure/Repositories/AccountRepository.cs
@@ -1,13 +1,26 @@
-﻿using BankingSystem.Domain.Entities;
+﻿using System.Diagnostics.Metrics;
+using BankingSystem.Domain.Entities;
 using BankingSystem.Domain.Interfaces.IRepositories;
 
 namespace BankingSystem.Infrastructure.Repositories
 {
     public class AccountRepository : IRepositoryAccount
     {
+        private readonly string connectionString;
+
+        public AccountRepository(string connectionString)
+        {
+            connectionString = connectionString;
+        }
+
+        public void AddAccount(Account account)
+        {
+           
+        }
+
         public List<Account> GetAccounts()
         {
-            throw new NotImplementedException();
+            throw NotImplementedException();
         }
     }
 }
--- BankingSystem.Infrastructure/Repositories/CustomerRepository.cs
+++ BankingSystem.Infrastructure/Repositories/CustomerRepository.cs
@@ -1,7 +1,25 @@
-﻿namespace BankingSystem.Infrastructure.Repositories
-{
-    public class CustomerRepository
-    {
+﻿using BankingSystem.Domain.Entities;
+using BankingSystem.Domain.Interfaces.IRepositories;
 
+namespace BankingSystem.Infrastructure.Repositories
+{
+    public class CustomerRepository : IRepositoryAccount
+    {
+        private readonly string _connectionString;
+
+        public CustomerRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void AddAccount(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Account> GetAccounts()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
