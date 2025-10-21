5551e9acb5de4116a9d65762836816cebe819259
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-21 12:04:50 +0200

    class Customer

--- BankingSystem.Domain/Entities/Customer.cs
+++ BankingSystem.Domain/Entities/Customer.cs
@@ -1,7 +1,26 @@
-﻿namespace BankingSystem.Domain.Entities
+﻿using System.Net;
+
+namespace BankingSystem.Domain.Entities
 {
     public class Customer
     {
+        public int CustomerId { get; set; }
 
+        public string FirstName { get; set; } = default!;
+        
+        public string LastName { get; set; } = default!;
+        
+        public string Street { get; set; } = default!;
+
+        public string HouseNumber { get; set; } = default!;;
+        
+        public string ZipCode { get; set; } = default!;;
+        
+        public string City { get; set; } = default!;;
+
+        public string Phone { get; set; } = default!;
+        
+        public string Email { get; set; } = default!;
     }
+
 }
