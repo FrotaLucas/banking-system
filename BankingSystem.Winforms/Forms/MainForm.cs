c5426eb96eb0aa509d4df05fd9e1b6e58c4620da
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-21 16:19:25 +0200

    create new forms

---
+++ BankingSystem.Winforms/Forms/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using BankingSystem.Domain.Interfaces.IRepositories;
+
+namespace BankingSystem.Winforms.Forms
+{
+    public partial class MainForm : Form
+    {
+        private readonly ICustomerRepository _customerRepo;
+        private readonly IAccountRepository _accountRepo;
+
+        public MainForm(ICustomerRepository customerRepo, IAccountRepository accountRepo)
+        {
+            InitializeComponent();
+            _customerRepo = customerRepo;
+            _accountRepo = accountRepo;
+           
+        }
+
+        private void btnTestGetCustomers_Click(object sender, EventArgs e)
+        {
+            DataTable dt = _customerRepo.GetAll();
+        }
+
+        private void btnCustomers_Click(object sender, EventArgs e)
+        {
+            //new CustomerForm(_customerRepo).ShowDialog();
+        }
+
+        private void btnAccounts_Click(object sender, EventArgs e)
+        {
+            //new AccountForm(_accountRepo).ShowDialog();
+        }
+    }
+}
--- BankingSystem.Winforms/Program.cs
+++ BankingSystem.Winforms/Program.cs
@@ -1,3 +1,4 @@
+using BankingSystem.Infrastructure;
 using BankingSystem.Winforms.Forms;
 
 namespace BankingSystem.Winforms
@@ -10,9 +11,18 @@ namespace BankingSystem.Winforms
         [STAThread]
         static void Main()
         {
+            string connString = "Data Source=.;Initial Catalog=BankDB;Integrated Security=True";
+            string connStringV2 = "Server=FROTAPC; database=bank_system-db; Trusted_connection=true; Trustservercertificate=true";
+            var db = new DbInitializer(connString);
+            db.InitializeDatabase();
+
+            BankDataSet dataSet = db.LoadInitialData();
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+
+
             Application.Run(new CustomerForm());
         }
     }
