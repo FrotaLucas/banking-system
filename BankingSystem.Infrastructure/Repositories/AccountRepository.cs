d82a75f68a119fbd7cbf7d97c2d0436b117766d2
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-21 18:58:04 +0200

    refactor AccountRepository

--- BankingSystem.Infrastructure/Repositories/AccountRepository.cs
+++ BankingSystem.Infrastructure/Repositories/AccountRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.Metrics;
+﻿using System.Data;
+using System.Diagnostics.Metrics;
 using BankingSystem.Domain.Entities;
 using BankingSystem.Domain.Interfaces.IRepositories;
 using Microsoft.Data.SqlClient;
@@ -7,34 +8,42 @@ namespace BankingSystem.Infrastructure.Repositories
 {
     public class AccountRepository : IAccountRepository
     {
-        private readonly string _connectionString;
 
-        public AccountRepository(string connectionString)
+        private readonly SqlDataAdapter _adapter;
+
+        private readonly BankDataSet _dataSet;
+
+        public AccountRepository(SqlConnection connection, BankDataSet dataSet)
         {
-            _connectionString = connectionString;
+           _dataSet = dataSet;
+           _adapter = new SqlDataAdapter("SELECT * FROM Accounts", connection);
         }
 
-        public void AddAccount(Account account)
-        {
-            using (SqlConnection conn = new SqlConnection(_connectionString))
-            {
-                conn.Open();
-                string sql = @"INSERT INTO Accounts (AccountNumber, CustomerId, OpeningBalance)
-                           VALUES (@AccountNumber, @CustomerId, @OpeningBalance)";
 
-                using (SqlCommand cmd = new SqlCommand(sql, conn))
-                {
-                    cmd.Parameters.AddWithValue("@AccountNumber", account.AccountNumber);
-                    cmd.Parameters.AddWithValue("@CustomerId", account.CustomerId);
-                    cmd.Parameters.AddWithValue("@OpeningBalance", account.Balance);
-                    cmd.ExecuteNonQuery();
-                }
-            }
-        }
+        public DataTable GetAll() => _dataSet.Accounts;
 
         public List<Account> GetAccounts()
         {
             return new List<Account>();
         }
+
+        public void AddAccount(Account account)
+        {
+            //using (SqlConnection conn = new SqlConnection(_connectionString))
+            //{
+            //    conn.Open();
+            //    string sql = @"INSERT INTO Accounts (AccountNumber, CustomerId, OpeningBalance)
+            //               VALUES (@AccountNumber, @CustomerId, @OpeningBalance)";
+
+            //    using (SqlCommand cmd = new SqlCommand(sql, conn))
+            //    {
+            //        cmd.Parameters.AddWithValue("@AccountNumber", account.AccountNumber);
+            //        cmd.Parameters.AddWithValue("@CustomerId", account.CustomerId);
+            //        cmd.Parameters.AddWithValue("@OpeningBalance", account.Balance);
+            //        cmd.ExecuteNonQuery();
+            //    }
+            //}
+        }
+
     }
 }
