f0abafd286de957d3403ea2ad1c441450381ddda
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-22 15:39:41 +0200

    implement table Customer

--- BankingSystem.Winforms/Forms/CustomerForm.Designer.cs
+++ BankingSystem.Winforms/Forms/CustomerForm.Designer.cs
@@ -7,10 +7,14 @@
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
+        private System.Windows.Forms.DataGridView dgvCustomers;
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.Label lblTitle;
+
         /// <summary>
-        /// Clean up any resources being used.
+        /// Clean up resources.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing"></param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,16 +26,71 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvCustomers = new System.Windows.Forms.DataGridView();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
+            this.SuspendLayout();
+
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(800, 40);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Customer List";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // 
+            // btnReload
+            // 
+            this.btnReload.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReload.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnReload.Location = new System.Drawing.Point(0, 40);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(800, 35);
+            this.btnReload.TabIndex = 1;
+            this.btnReload.Text = "Reload";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+
+            // 
+            // dgvCustomers
+            // 
+            this.dgvCustomers.AllowUserToAddRows = false;
+            this.dgvCustomers.AllowUserToDeleteRows = false;
+            this.dgvCustomers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvCustomers.Location = new System.Drawing.Point(0, 75);
+            this.dgvCustomers.Name = "dgvCustomers";
+            this.dgvCustomers.ReadOnly = true;
+            this.dgvCustomers.RowHeadersVisible = false;
+            this.dgvCustomers.RowTemplate.Height = 25;
+            this.dgvCustomers.Size = new System.Drawing.Size(800, 375);
+            this.dgvCustomers.TabIndex = 2;
+
+            // 
+            // CustomerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvCustomers);
+            this.Controls.Add(this.btnReload);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CustomerForm";
             this.Text = "CustomerForm";
+
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
+            this.ResumeLayout(false);
         }
 
         #endregion
--- BankingSystem.Winforms/Forms/CustomerForm.cs
+++ BankingSystem.Winforms/Forms/CustomerForm.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using BankingSystem.Domain.Interfaces.IRepositories;
 
@@ -14,10 +8,41 @@ namespace BankingSystem.Winforms.Forms
     public partial class CustomerForm : Form
     {
         private readonly ICustomerRepository _customerRepo;
+
         public CustomerForm(ICustomerRepository customerRepo)
         {
             InitializeComponent();
             _customerRepo = customerRepo;
+
+            // Carrega os dados na inicialização
+            LoadCustomers();
+        }
+
+        /// <summary>
+        /// Carrega os dados de clientes no DataGridView.
+        /// </summary>
+        private void LoadCustomers()
+        {
+            try
+            {
+                DataTable customers = _customerRepo.GetAll();
+                dgvCustomers.DataSource = customers;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading customers: {ex.Message}",
+                                "Error",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Evento do botão Reload.
+        /// </summary>
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            LoadCustomers();
         }
     }
 }
