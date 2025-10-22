8ff28978cdf0954f2511bd303bc18cce2ee12be1
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-22 15:39:06 +0200

    implement tab control in MainForm

--- BankingSystem.Winforms/Forms/MainForm.Designer.cs
+++ BankingSystem.Winforms/Forms/MainForm.Designer.cs
@@ -6,7 +6,11 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-    
+        private TabControl tabControlMain;
+        private TabPage tabPageCustomers;
+        private TabPage tabPageAccounts;
+        private Panel panelContent;
+
 
         /// <summary>
         /// Clean up any resources being used.
@@ -21,8 +25,6 @@
             base.Dispose(disposing);
         }
 
-
-
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -32,9 +34,63 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.tabControlMain = new System.Windows.Forms.TabControl();
+            this.tabPageCustomers = new System.Windows.Forms.TabPage();
+            this.tabPageAccounts = new System.Windows.Forms.TabPage();
+            this.panelContent = new System.Windows.Forms.Panel();
+            this.tabControlMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControlMain
+            // 
+            this.tabControlMain.Controls.Add(this.tabPageCustomers);
+            this.tabControlMain.Controls.Add(this.tabPageAccounts);
+            this.tabControlMain.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControlMain.Location = new System.Drawing.Point(0, 0);
+            this.tabControlMain.Name = "tabControlMain";
+            this.tabControlMain.SelectedIndex = 0;
+            this.tabControlMain.Size = new System.Drawing.Size(800, 60);
+            this.tabControlMain.TabIndex = 0;
+            this.tabControlMain.SelectedIndexChanged += new System.EventHandler(this.TabControlMain_SelectedIndexChanged);
+            // 
+            // tabPageCustomers
+            // 
+            this.tabPageCustomers.Location = new System.Drawing.Point(4, 24);
+            this.tabPageCustomers.Name = "tabPageCustomers";
+            this.tabPageCustomers.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageCustomers.Size = new System.Drawing.Size(792, 32);
+            this.tabPageCustomers.TabIndex = 0;
+            this.tabPageCustomers.Text = "Customers";
+            this.tabPageCustomers.UseVisualStyleBackColor = true;
+            // 
+            // tabPageAccounts
+            // 
+            this.tabPageAccounts.Location = new System.Drawing.Point(4, 24);
+            this.tabPageAccounts.Name = "tabPageAccounts";
+            this.tabPageAccounts.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageAccounts.Size = new System.Drawing.Size(792, 32);
+            this.tabPageAccounts.TabIndex = 1;
+            this.tabPageAccounts.Text = "Accounts";
+            this.tabPageAccounts.UseVisualStyleBackColor = true;
+            // 
+            // panelContent
+            // 
+            this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContent.Location = new System.Drawing.Point(0, 60);
+            this.panelContent.Name = "panelContent";
+            this.panelContent.Size = new System.Drawing.Size(800, 390);
+            this.panelContent.TabIndex = 1;
+            // 
+            // MainForm
+            // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainForm";
+            this.Controls.Add(this.panelContent);
+            this.Controls.Add(this.tabControlMain);
+            this.Name = "MainForm";
+            this.Text = "Banking System";
+            this.tabControlMain.ResumeLayout(false);
+            this.ResumeLayout(false);
         }
 
         #endregion
--- BankingSystem.Winforms/Forms/MainForm.cs
+++ BankingSystem.Winforms/Forms/MainForm.cs
@@ -8,13 +8,57 @@ namespace BankingSystem.Winforms.Forms
         private readonly ICustomerRepository _customerRepo;
         private readonly IAccountRepository _accountRepo;
 
+        private CustomerForm? _customerForm;
+        private AccountForm? _accountForm;
+
         public MainForm(ICustomerRepository customerRepo, IAccountRepository accountRepo)
         {
             InitializeComponent();
             _customerRepo = customerRepo;
             _accountRepo = accountRepo;
-           
+
+            // Mostra a aba inicial (Customers)
+            ShowCustomerForm();
         }
+
+        private void TabControlMain_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControlMain.SelectedTab == tabPageCustomers)
+            {
+                ShowCustomerForm();
+            }
+            else if (tabControlMain.SelectedTab == tabPageAccounts)
+            {
+                ShowAccountForm();
+            }
+        }
+
+        private void ShowCustomerForm()
+        {
+            panelContent.Controls.Clear();
+
+            _customerForm ??= new CustomerForm(_customerRepo);
+            _customerForm.TopLevel = false;
+            _customerForm.FormBorderStyle = FormBorderStyle.None;
+            _customerForm.Dock = DockStyle.Fill;
+
+            panelContent.Controls.Add(_customerForm);
+            _customerForm.Show();
+        }
+
+        private void ShowAccountForm()
+        {
+            panelContent.Controls.Clear();
+
+            _accountForm ??= new AccountForm(_accountRepo);
+            _accountForm.TopLevel = false;
+            _accountForm.FormBorderStyle = FormBorderStyle.None;
+            _accountForm.Dock = DockStyle.Fill;
+
+            panelContent.Controls.Add(_accountForm);
+            _accountForm.Show();
+        }
+
 
         private void btnCustomers_Click(object sender, EventArgs e)
         {
