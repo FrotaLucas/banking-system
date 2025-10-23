462b1487310283b11f00fa413f809d090415d363
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-23 21:44:48 +0200

    implement functions to AccountForm

--- BankingSystem.Winforms/Forms/AccountForm.Designer.cs
+++ BankingSystem.Winforms/Forms/AccountForm.Designer.cs
@@ -2,15 +2,13 @@
 {
     partial class AccountForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dgvAccounts;
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.DataGridViewButtonColumn colDelete;
+        private System.Windows.Forms.Button btnAddAccount;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,19 +19,93 @@
         }
 
         #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvAccounts = new System.Windows.Forms.DataGridView();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnAddAccount = new System.Windows.Forms.Button();
+            this.colDelete = new System.Windows.Forms.DataGridViewButtonColumn();
+
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAccounts)).BeginInit();
+            this.SuspendLayout();
+
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(800, 40);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Account List";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // 
+            // btnReload
+            // 
+            this.btnReload.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReload.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(800, 35);
+            this.btnReload.TabIndex = 1;
+            this.btnReload.Text = "Reload";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+
+            // 
+            // btnAddAccount
+            // 
+            this.btnAddAccount.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnAddAccount.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAddAccount.Name = "btnAddAccount";
+            this.btnAddAccount.Size = new System.Drawing.Size(800, 35);
+            this.btnAddAccount.TabIndex = 2;
+            this.btnAddAccount.Text = "Add Account";
+            this.btnAddAccount.UseVisualStyleBackColor = true;
+            this.btnAddAccount.Click += new System.EventHandler(this.btnAddAccount_Click);
+
+            // 
+            // dgvAccounts
+            // 
+            this.dgvAccounts.AllowUserToAddRows = false;
+            this.dgvAccounts.AllowUserToDeleteRows = false;
+            this.dgvAccounts.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvAccounts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAccounts.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvAccounts.Name = "dgvAccounts";
+            this.dgvAccounts.ReadOnly = true;
+            this.dgvAccounts.RowHeadersVisible = false;
+            this.dgvAccounts.RowTemplate.Height = 25;
+            this.dgvAccounts.TabIndex = 3;
+            this.dgvAccounts.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAccounts_CellContentClick);
+
+            // 
+            // colDelete
+            // 
+            this.colDelete.HeaderText = "Delete";
+            this.colDelete.Name = "colDelete";
+            this.colDelete.Text = "Delete";
+            this.colDelete.UseColumnTextForButtonValue = true;
+            this.colDelete.ReadOnly = true;
+
+            // 
+            // AccountForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvAccounts);
+            this.Controls.Add(this.btnAddAccount);
+            this.Controls.Add(this.btnReload);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AccountForm";
             this.Text = "AccountForm";
-        }
 
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAccounts)).EndInit();
+            this.ResumeLayout(false);
+        }
         #endregion
     }
 }
--- BankingSystem.Winforms/Forms/AccountForm.cs
+++ BankingSystem.Winforms/Forms/AccountForm.cs
@@ -1,23 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Data;
 using BankingSystem.Domain.Interfaces.IRepositories;
 
 namespace BankingSystem.Winforms.Forms
 {
     public partial class AccountForm : Form
     {
-        private readonly IAccountRepository accountRepo;
+        private readonly IAccountRepository _accountRepo;
+
         public AccountForm(IAccountRepository accountRepo)
         {
             InitializeComponent();
-            this.accountRepo = accountRepo;
+            _accountRepo = accountRepo;
+
+            LoadAccounts();
+        }
+
+        private void LoadAccounts()
+        {
+            try
+            {
+                DataTable accounts = _accountRepo.GetAll();
+                dgvAccounts.DataSource = accounts;
+
+                if (dgvAccounts.Columns["colDelete"] == null)
+                    dgvAccounts.Columns.Add(colDelete);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading accounts: {ex.Message}",
+                                "Error",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnReload_Click(object sender, EventArgs e)
+        {
+            LoadAccounts();
+        }
+
+        private void btnAddAccount_Click(object sender, EventArgs e)
+        {
+            using var form = new AccountCreateForm(_accountRepo);
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                LoadAccounts();
+            }
+        }
+
+        private void dgvAccounts_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+                return;
+
+            if (dgvAccounts.Columns[e.ColumnIndex].Name == "colDelete")
+            {
+                var accountId = (int)dgvAccounts.Rows[e.RowIndex].Cells["Id"].Value;
+                var confirm = MessageBox.Show("Are you sure you want to delete this account?",
+                                              "Confirm Delete",
+                                              MessageBoxButtons.YesNo,
+                                              MessageBoxIcon.Warning);
+
+                if (confirm == DialogResult.Yes)
+                {
+                    _accountRepo.Delete(accountId);
+                    MessageBox.Show("Account deleted successfully!", "Success",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadAccounts();
+                }
+            }
         }
     }
 }
