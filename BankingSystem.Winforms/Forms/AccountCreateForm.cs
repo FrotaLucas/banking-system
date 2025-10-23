223f0392d05430a1bbdf5a9671a8db98acb3effd
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-23 21:43:57 +0200

    create new form

---
+++ BankingSystem.Winforms/Forms/AccountCreateForm.cs
@@ -0,0 +1,16 @@
+﻿using BankingSystem.Domain.Interfaces.IRepositories;
+
+namespace BankingSystem.Winforms.Forms
+{
+    public partial class AccountCreateForm : Form
+    {
+
+        private readonly IAccountRepository _repo;
+
+        public AccountCreateForm(IAccountRepository repo)
+        {
+            InitializeComponent();
+            _repo = repo;
+        }
+    }
+}
