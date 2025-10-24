247f2677330d8b8760ec31f0468bb69759141396
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-25 00:59:28 +0200

    add new entity

---
+++ BankingSystem.Domain/Entities/Transaction.cs
@@ -0,0 +1,20 @@
+﻿namespace BankingSystem.Domain.Entities
+{
+    public class Transaction
+    {
+        int Id { get; set; }    
+
+        public DateTime DateTime { get; set; } = DateTime.Now;
+
+        public string TransactionType { get; set; } = default!;
+
+        public decimal Amount { get; set; } = default!;
+
+        public string Purpose { get; set; } = default!;
+
+        public string IBAN { get; set; } = default!;
+
+        public string TransactionNumber { get; set; }  = default!;
+
+    }
+}
