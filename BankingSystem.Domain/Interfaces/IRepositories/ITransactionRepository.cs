9d6e0ecaf55b5740837780deb1bbccaf9ee01169
FrotaLucas <lucasdiasfrota@gmail.com>
2025-10-25 12:58:17 +0200

    crete TransactionRepository

---
+++ BankingSystem.Domain/Interfaces/IRepositories/ITransactionRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+using System.Transactions;
+
+namespace BankingSystem.Domain.Interfaces.IRepositories
+{
+    public interface ITransactionRepository
+    {
+        void AddNewTransaction(Transaction transaction);
+
+        DataTable GetTransactionsByAccountId();
+    }
+}
---
+++ BankingSystem.Infrastructure/Repositories/TransactionRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using System.Transactions;
+using BankingSystem.Domain.Interfaces.IRepositories;
+using Microsoft.Data.SqlClient;
+
+namespace BankingSystem.Infrastructure.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+
+
+        private readonly SqlDataAdapter _adapter;
+
+        private readonly BankDataSet _dataSet;
+
+        private readonly IAccountRepository accountRepository;
+
+        public TransactionRepository(SqlConnection connection, BankDataSet dataSet, IAccountRepository accountRepository)
+        {
+            _dataSet = dataSet;
+            _adapter = new SqlDataAdapter("SELECT * FROM Transactions", connection);
+
+            var builder = new SqlCommandBuilder(_adapter);
+            accountRepository = accountRepository;
+        }
+
+
+        public void AddNewTransaction(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetTransactionsByAccountId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
